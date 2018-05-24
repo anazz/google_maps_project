6dac35017d66e084577cfe87b79f668e0b2048d5
Ana Zegheanu <ana.zegheanu@gmail.com>
2018-05-24 16:56:42 +0200

    add project

---
+++ README.md
@@ -0,0 +1,17 @@
+# Hello mon correcteur, 
+
+L'application "google_maps_projects" affiche une location et un message, sur le plan choisie par le user.
+
+Afin de visualiser le plan, choisir un endroit et avoir un message sur le marker, il suffit de remplir le petit formulaire avec 
+une latitude, une longitude et le message.
+
+Quand tu vas passer la souris sur le marker de google maps, tu vas voir ton super message.
+
+J'espère que tu vas aimer cette experience. 
+
+Merci pour ta correction 💛 💚 💙 💜
+
+
+
+* Ruby version 2.3.4
+
---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,38 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
+
+function initMap() {
+
+   var latitude = parseFloat(document.getElementById("latitude").value);
+   var longitude = parseFloat(document.getElementById("longitude").value);
+   var message = document.getElementById("message").value;
+   // var uluru = {lat: -25.363, lng: 131.044};
+   var uluru = {lat: latitude, lng: longitude};
+   var map = new google.maps.Map(document.getElementById("map"), {
+   zoom: 4,
+   center: uluru
+});
+
+   var marker = new google.maps.Marker({
+   position: uluru,
+   map: map,
+   title: message
+});
+}
+
+
